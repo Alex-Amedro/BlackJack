548da03be644e0a9da92765ff06b60b7282eb3a5
alexandre <alex.amedro@gmail.com>
2026-03-21 11:55:10 +0100

    start

---
+++ src/test/java/n7/fr/BlackJack/BlackJackApplicationTests.java
@@ -0,0 +1,13 @@
+package n7.fr.BlackJack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackJackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
